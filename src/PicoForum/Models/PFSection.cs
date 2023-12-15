46c58321c48102c3963d33b0e043d05ee697b569
kingsznhone <35349300+kingsznhone@users.noreply.github.com>
2023-12-16 01:12:43 +0800

    move file

--- src/PicoForum/Models/PFSection.cs
+++ src/PicoForum/Models/PFSection.cs
@@ -1,8 +1,16 @@
-﻿namespace PicoForum.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PicoForum.Models
 {
     public class PFSection
     {
         public int PFSectionId { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:O}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Update Time")]
+        public DateTime? TimeUpdate { get; set; }
+
     }
 }
